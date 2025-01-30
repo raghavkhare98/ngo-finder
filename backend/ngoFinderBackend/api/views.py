e4fdebe76bad16858a6cc1fc058f61783ecc6754
raghavkhare98 <khare08rma@gmail.com>
2025-01-30 16:57:51 -0500

    Add views

--- backend/ngoFinderBackend/api/views.py
+++ backend/ngoFinderBackend/api/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
+from rest_framework import generics
+from .models import City, NGO
+from .serializers import CitySerializer, NGOSerializer
 
-# Create your views here.
+class CityListCreate(generics.ListCreateAPIView):
+    queryset = City.objects.all()
+    serializer_class = CitySerializer
+
+class NGOListCreate(generics.ListCreateAPIView):
+    queryset = NGO.objects.all()
+    serializer_class = NGOSerializer
+
+class NGOsByCity(generics.ListAPIView):
+
+    serializer_class = NGOSerializer
+
+    def get_queryset(self):
+        city_name = self.kwargs['city']
+        return NGO.objects.filter(city__name=city_name)
