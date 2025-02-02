dc7b22cbe0693bfa9a00dd9db643c92d079a4bdc
raghavkhare98 <khare08rma@gmail.com>
2025-02-02 15:29:05 -0500

    Corrected seed.py location from project to the app

--- backend/ngoFinderBackend/api/management/commands/seeding.py
+++ backend/ngoFinderBackend/api/management/commands/seeding.py
@@ -2,9 +2,12 @@
 from django.core.management.base import BaseCommand
 import random
 from api.models import NGO, City
+import logging
+from faker import Faker
 
 MODE_REFRESH = 'refresh' #refreshes the db by clearing all previous data and creating new addresses
 MODE_CLEAR = 'clear' #just clears the data
+fake = Faker()
 
 class Command(BaseCommand):
     
@@ -17,15 +20,26 @@ class Command(BaseCommand):
         self.stdout.write("seeding completed")
 
 def clear_data():
+    logging.info('Clearing/Deleting data objects')
     NGO.objects.all().delete()
     City.objects.all().delete()
 
-def create_addresses():
-    pass
+def create_ngos():
+    logging.info('Creating data objects')
+    cities = ['Waterloo', 'Kitchener', 'Cambridge', 'Guelph']
+    ngos = NGO(
+        ngo_name=fake.name(),
+        description=fake.text(),
+        city_name=random.choice(cities)
+    )
+    ngos.save()
+    logging.info('Creation complete')
+    return ngos
 
+    
 def run_seed(self, mode):
     clear_data()
     if mode == MODE_CLEAR:
         return
-    for i in range(20):
-        create_addresses()
+    for _ in range(20):
+        create_ngos()
