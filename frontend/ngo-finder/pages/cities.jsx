94b4f2fd2f1643bfae6a31ef1f4478d676d3d208
raghavkhare98 <khare08rma@gmail.com>
2025-03-27 18:18:54 -0400

    Remove button from cities page and add list of cities

--- frontend/ngo-finder/pages/cities.jsx
+++ frontend/ngo-finder/pages/cities.jsx
@@ -2,23 +2,36 @@ import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
 import Head from "next/head"
 import styles from "../styles/cities.module.css"
-import Link from "next/link";
 
-const cities = ["Waterloo", "Kitchener", "Cambridge", "Guelph"]
-
-export default function Cities({repo}){
-
-    /*
-    WILL WORK ON THIS WHEN WE ADD MORE CITIES
-    
-    async function getCityName(){
-        const response = await fetch("http://0.0.0.0:8000/api/cities/");
-        const data = await response.json();
-        console.log(data);
+export async function getServerSideProps(){
+    try{
+        const reponse = await fetch("http://0.0.0.0:8000/api/cities/");
+        const cities = await reponse.json();
+        return{
+            props: {
+                cities: cities
+            }
+        };
+    } catch(error){
+        return{
+            props:{
+                cities: []
+            }
+        };
     }
-    const cityNames = getCityName().then()*/
+}
+
+export default function Cities({ cities }){
+    
+    function capitalizeLetter(word){
+        let splitStr = word.toLowerCase().split(' ');
+        for(let i=0; i < splitStr.length; i++){
+            splitStr[i] = splitStr[i].charAt(0).toUpperCase() + splitStr[i].substring(1);
+        }
+        return splitStr.join(' ');
+    }
+
     return(
-        
         <div className={styles.container}>
             <Head>
                 <title>Careconnect | Cities</title>
@@ -27,11 +40,12 @@ export default function Cities({repo}){
             <main className={styles.content}>
                 <h1 className={styles.title}>Select your city</h1>
                 <div className={styles.city_list}>
-                    {cities.map((city => (
-                        <button><Link key={city} href={`/ngo-list?city=${city}`}>  
-                            {city}
-                        </Link></button>
-                    )))}
+                    <ul>
+                        {cities.map((city, index) => {
+                            let cityName = capitalizeLetter(city.city_name);
+                            return <li key={index}>{cityName}</li>;
+                        })}
+                    </ul>
                 </div>
             </main>
             <Footer />
